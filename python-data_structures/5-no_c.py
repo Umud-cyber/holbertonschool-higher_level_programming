84d9f445521d02ef241174dea99c4b2b4cb82ee1
Your Name <you@example.com>
2025-11-28 17:18:50 +0000

    Task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_list = ""
+    new_string = ""
     for ch in my_string:
-        if ch != "c" and ch != "C":
+        if ch != 'c' and ch != 'C':
             new_string += ch
     return new_string
-
