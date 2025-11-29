baaffe701a79a3fde00eb8c53c75714e68bae9a3
Your Name <you@example.com>
2025-11-29 13:39:49 +0000

    task 10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -4,6 +4,6 @@ def divisible_by_2(my_list=[]):
     for num in my_list:
         if num % 2 == 0:
             new_list.append(True)
-        else :
+        else:
             new_list.append(False)
     return new_list
