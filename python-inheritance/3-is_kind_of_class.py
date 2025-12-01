5d128f042061389c072c8039c870e80eb0d50f37
Your Name <you@example.com>
2025-12-01 12:10:58 +0000

    Task 3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ a function that returns True if the object is an rited from """
+
+
 def is_kind_of_class(obj, a_class):
     """ let's check"""
     return isinstance(obj, a_class)
