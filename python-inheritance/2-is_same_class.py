500c07e60dee9becf4b46bf7ecfb863c47ed45bd
Your Name <you@example.com>
2025-12-01 11:56:24 +0000

    Task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def is_same_class(obj, a_class):
-     """ a function that returns True if the object is exactly an instance of the specified class ; otherwise False """
-
-
+    """Return True if obj is exactly an instance of a_class; otherwise False."""
     return type(obj) == a_class
