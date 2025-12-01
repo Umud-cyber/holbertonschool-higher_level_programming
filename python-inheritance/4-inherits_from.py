16b2b85a017a80684c9c38dbe53e0ea382d57b3e
Your Name <you@example.com>
2025-12-01 14:40:11 +0000

    Task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+""" THdkjdikdadlkbdklvksdokbs djfs"""
+
+
 def inherits_from(obj, a_class):
+    """dhfh dijd"""
     return type(obj) is not a_class and  issubclass(type(obj), a_class)
