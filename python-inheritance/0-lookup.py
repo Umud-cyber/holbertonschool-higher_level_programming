d5267cad35ac5d8414e25089ae864348ced6fd0f
Your Name <you@example.com>
2025-12-01 09:10:27 +0000

    Task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Module that defines a function for looking up attributes and methods of an object."""
+
 def lookup(obj):
     """Return the list of available attributes and methods of an object."""
     return dir(obj)
