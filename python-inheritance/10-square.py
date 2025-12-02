d6ca10b0bdfae3a76e1eb5c2e7aa2865ef35c117
Your Name <you@example.com>
2025-12-02 15:19:23 +0000

    TAsk 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""fjjff fjfnfj ffkfn """
+
+
+class Square(Rectangle):
+    """ fhf dhhf fjf """
+
+    def __init__(self, size):
+        """ fh fhdn djdfdjjd"""
+        self.integer_validator("size", size)
+        self.__size = size
+        
+
+        def area(self):
+            """ dhnfd ifmnfdm fjhfnme"""
+            return self.__size * self.__size
