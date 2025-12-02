299305cd436aa141ad55abd58794684146b8a1b3
Your Name <you@example.com>
2025-12-02 17:56:51 +0000

    Task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module that defines a function to write text to a UTF-8 file."""
 
+
 def write_file(filename="", text=""):
     """Writes text to a file and returns number of characters written."""
     with open(filename, "w", encoding="utf-8") as f:
