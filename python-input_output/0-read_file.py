2683aedb6f344f827e71bf50e4ffdbd6987670f9
Your Name <you@example.com>
2025-12-02 17:07:52 +0000

    Task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python3
 def read_file(filename=""):
-    """ dhhnd djff jd fjd"""
+    """Reads a UTF-8 text file and prints its content to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
-
