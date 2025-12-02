acc40e5738cba3e444042e5652301ab7c1e509f1
Your Name <you@example.com>
2025-12-02 17:21:52 +0000

    Task 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" dkjmf kdmf,m dmglke"""
+
+
+def write_file(filename="", text=""):
+    """ ye fhff fjfd mdjkjdfelkm,s vl"""
+    with open(filename, "w", encoding="utf-8") as f:
+        print(f.read(), end="")
+
