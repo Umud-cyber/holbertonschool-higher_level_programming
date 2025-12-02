cf79f012cc2e8eca5d458e1b85e068773c700e63
Your Name <you@example.com>
2025-12-02 17:10:26 +0000

    Task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module that defines a function to read and print a UTF-8 text file."""
+  
+
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its content to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
