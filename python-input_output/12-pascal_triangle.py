3396a5e6cb77aee3901b7d83d488e3b63d593981
Your Name <you@example.com>
2025-12-03 16:05:07 +0000

    Task 12

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+def pascal_triangle(n):
+    """Return Pascal's triangle of n rows."""
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        # hər sətrin başlanğıcı
+        row = [1]
+
+        # ortadakı elementlər
+        if i > 0:
+            prev = triangle[i - 1]
+            for j in range(1, i):
+                row.append(prev[j - 1] + prev[j])
+            row.append(1)
+
+        triangle.append(row)
+
+    return triangle
