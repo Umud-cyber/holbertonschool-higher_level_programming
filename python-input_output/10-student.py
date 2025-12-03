0424ddb5ef2d729a832e0321759c2cdc7f62a864
Your Name <you@example.com>
2025-12-03 15:48:30 +0000

    Task 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-""" dhd jhjdf djf"""
+"""Defines a simple Student class."""
 
-class Student():
-    """ jfdfd jxdfgbkx"""
 
+class Student:
+    """Student model."""
 
     def __init__(self, first_name, last_name, age):
-        """ sjkdd fzhjf """
+        """Initialize attributes."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """ dnzf """
+    def to_json(self, attrs=None):
+        """Return dictionary representation of the student."""
+        if type(attrs) is list:
+            new_dict = {}
+            for key in attrs:
+                if type(key) is str and key in self.__dict__:
+                    new_dict[key] = self.__dict__[key]
+            return new_dict
+
         return self.__dict__
-    if type(attrs) is list:
-        new = {}
-        for key in attrs:
-            if key in self.__dict__:
-                new[key] = self.__dict__[key]
-        return new
-    return self.__dict__
