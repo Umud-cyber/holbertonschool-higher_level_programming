cc5f5f27b866f23a48393011cbd47bb15e0ccca1
Your Name <you@example.com>
2025-12-03 06:10:13 +0000

    TAsk 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
 def append_write(filename="", text=""):
-    """ hd dbbcc djjd cbjd dk
-    kdmdkd dkdm  odmwl dwkmdmw """
-
+    """
+    Appends a string at the end of a text file (UTF8)
+    and returns the number of characters added.
+    """
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
