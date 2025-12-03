5fdd95cc73621053de384da197836abcddf44b2f
Your Name <you@example.com>
2025-12-03 07:07:24 +0000

    Task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-""" m,dsf  kkfjk,dnks """
-
+"""Add all arguments to a Python list and save them to a file."""
 import sys
-import json
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 
@@ -14,5 +12,4 @@ except Exception:
     items = []
 
 items.extend(sys.argv[1:])
-
 save_to_json_file(items, filename)
