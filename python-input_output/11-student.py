94b410ae1bc8c7fd276a5220829afb20f3435c44
Your Name <you@example.com>
2025-12-03 08:19:36 +0000

    Task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" dhd jhjdf djf"""
+
+class Student():
+    """ jfdfd jxdfgbkx"""
+
+
+    def __init__(self, first_name, last_name, age):
+        """ sjkdd fzhjf """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self):
+        """ dnzf """
+        return self.__dict__
+    if type(attrs) is list:
+        new = {}
+        for key in attrs:
+            if key in self.__dict__:
+                new[key] = self.__dict__[key]
+        return new
+    def reload_from_json(self, json):
+        """ jf dkfjn dukhg """
+        for key, value in json.items():
+            self.__dict__[key] = value
