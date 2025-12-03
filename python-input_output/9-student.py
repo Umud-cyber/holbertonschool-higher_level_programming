50c83b36bb8a0c6cf30760b406f18adc22f11cc6
Your Name <you@example.com>
2025-12-03 15:38:39 +0000

    Task 9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -4,12 +4,12 @@
 class Student():
     """ jfdfd jxdfgbkx"""
 
-
     def __init__(self, first_name, last_name, age):
         """ sjkdd fzhjf """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
         """ dnzf """
         return self.__dict__
