d0a435371d341f417a4ab4bffa323277e38c1a0f
Your Name <you@example.com>
2025-12-03 06:41:39 +0000

    Task 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,5 +6,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """ jdj uiabcdj cjklac"""
-    with open(filename, "w", encoding="utf-8") as f
+    with open(filename, "w", encoding="utf-8") as f:
     return file.write(json.dumps(my_obj))
