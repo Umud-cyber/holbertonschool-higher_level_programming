035e06343c497743f01b2723919309bbe8bcc161
Your Name <you@example.com>
2025-12-04 09:39:38 +0000

    Task 5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
--- asjfkesf
-SELECT COLUMN_NAME, COLUMN_TYPE
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0'
-  AND table_name = 'first_table';
+-- dszkgsz
+SHOW CREATE TABLE first_table;
