ef8ad6da6cf45b9dac9fa00daf9aa28d79c441f2
Your Name <you@example.com>
2025-12-04 09:37:32 +0000

    Task 5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,5 @@
--- kdszlfnjds sdzofnsdzv
-SELECT hbtn_0c_0
-FROM first_table
+-- asjfkesf
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM information_schema.columns
+WHERE table_schema = 'hbtn_0c_0'
+  AND table_name = 'first_table';
