9499176d8931b77e2a0e46fd3d18316c444eb762
Your Name <you@example.com>
2025-12-04 15:49:53 +0000

    Task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,8 @@
---skle sd
+-- script that lists all cities contained in the database hbtn_0d_usa.
+-- Each record displays: cities.id - cities.name - states.name
+-- results sortes in ascending order by cities.id
+-- used only one SELECT statement
 SELECT cities.id, cities.name, states.name
-FROM cities, states
-WHERE cities.state_id = states.id
+FROM cities
+LEFT JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
