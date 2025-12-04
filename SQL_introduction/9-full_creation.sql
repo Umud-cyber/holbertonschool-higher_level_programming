6ecc88639cf62f4574be92b14dfbaa177f5fcac0
Your Name <you@example.com>
2025-12-04 11:00:52 +0000

    Task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,6 @@
 -- dzjfdsk
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+-- szjdei
 INSERT INTO second_table (id, name, score)
 VALUES (1, "John", 10
         2, "Alex", 3
