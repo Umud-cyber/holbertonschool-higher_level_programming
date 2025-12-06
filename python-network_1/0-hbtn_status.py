0b7cb91ec03859fcca4ee0b5f04074fe1a3226f9
Your Name <you@example.com>
2025-12-06 09:36:18 +0000

    Task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import urllib.request
+if __name__ == "__main__":
 with urllib.request.urlopen(https://intranet.hbtn.io/status) as response:
     body = response.read()
+    print("Body response:")
     print("\t- type: {}".format(type(body)))
     print("\t- content: {}".format(body)))
     print("\t- utf8 content: {}".format(body.decode('utf-8')))
