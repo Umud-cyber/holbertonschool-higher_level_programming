3c4244dbb84e3f6b0690e79a35fccdc83eb3426a
Your Name <you@example.com>
2025-12-06 10:38:02 +0000

    Task 1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,7 +2,8 @@
 import sys
 import urllib.request
 if __name__ == "__main__":
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    header = response.getheader('X-Request-Id')
-    print("\t- value: {}".format(header))
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        header = response.getheader('X-Request-Id')
+        print("\t- value: {}".format(header))
+~                                             
