7ce9c214d6c37d3cea1177347fa16fe1a72d31a1
Your Name <you@example.com>
2025-12-06 09:37:16 +0000

    Task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
+"""
+this module fetches https://intranet.hbtn.io/status using the urllib package.
+it demonstrates how to make a simple GET request and display response details.
+"""
 import urllib.request
+
 if __name__ == "__main__":
-with urllib.request.urlopen(https://intranet.hbtn.io/status) as response:
-    body = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body)))
-    print("\t- utf8 content: {}".format(body.decode('utf-8')))
+    url = 'https://intranet.hbtn.io/status'
+
+    # request
+    # it uses 'with' to ensure the connection is closed automatically
+    with urllib.request.urlopen(url) as response:
+        # read the body
+        body = response.read()
+
+        # display the details
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8'))
